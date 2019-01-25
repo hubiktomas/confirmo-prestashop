006a2617a5bdcf0e1f3647a39a9510983511fb51
Tomáš Hubík <hubik.tomas@gmail.com>
2019-01-25 12:32:01 +0100

    Typo

--- controllers/front/notification.php
+++ controllers/front/notification.php
@@ -201,7 +201,7 @@ class ConfirmoNotificationModuleFrontController extends ModuleFrontController
      *
      * @param string $message error message
      * @param string $dataString callback string
-     * @param bool $die halts the whole exection after writing to log if true
+     * @param bool $die halts the whole execution after writing to log if true
      */
     public function error($message, $dataString = "", $die = true)
     {
